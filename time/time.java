4a70ac82e72bfc318d2fe6debd488625c884e829
Adrián Silva <adri4silva@gmail.com>
2016-12-12 22:38:47 +0100

    Cambiados nombres erroneos

--- time/time.java
+++ time/time.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ejemplo_this;
+package time;
 import java.time.Duration;
 import java.time.Instant;
 import javax.swing.JOptionPane;
@@ -11,7 +11,7 @@ import javax.swing.JOptionPane;
  *
  * @author Adri
  */
-public class Ejemplo_This {
+public class time {
 
     /**
      * @param args the command line arguments
