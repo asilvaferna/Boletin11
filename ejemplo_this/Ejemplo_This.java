07a185a078d0fafe79d032b87fa38a20fe69bb1b
Adrián Silva <adri4silva@gmail.com>
2016-12-12 22:37:16 +0100

    Subido boletin 11

---
+++ ejemplo_this/Ejemplo_This.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo_this;
+import java.time.Duration;
+import java.time.Instant;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Adri
+ */
+public class Ejemplo_This {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            
+        String entrada = "";
+        
+       /* long principio = System.currentTimeMillis();
+        entrada = JOptionPane.showInputDialog("A documentación é todo aquel conxunto de manuais impresos ou en formato dixital que explique unha aplicación informática .");
+        double fin = System.currentTimeMillis();
+        JOptionPane.showMessageDialog(null, "Tiempo: " + ((fin - principio)/1000) + " segundos." );*/
+        Instant ahora = Instant.now();
+        String entrada2 = JOptionPane.showInputDialog("Hola");
+        Instant despues = Instant.now();
+        Duration.between(ahora, despues).getSeconds();
+        //JOptionPane.showMessageDialog(null, Duration.between(ahora, despues));
+        System.out.println(Duration.between(ahora, despues).getSeconds());
+        
+        
+            
+    }
+    
+}
